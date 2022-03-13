cf54ab56105c410a4f869d1fc7f4236a735e764c
Indigo <durovic.marek@gmail.com>
2022-03-13 18:38:07 +0100

    profiles with h2 db working

--- endpoints/src/main/java/sk/durovic/core/CoreApplication.java
+++ endpoints/src/main/java/sk/durovic/core/CoreApplication.java
@@ -1,13 +1,31 @@
 package sk.durovic.core;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class CoreApplication {
 
+	@Autowired
+	private Environment env;
+
 	public static void main(String[] args) {
 		SpringApplication.run(CoreApplication.class, args);
+
+	}
+
+	@Bean
+	public String getProfile(){
+
+		for (String profile : env.getActiveProfiles()
+			 ) {
+			System.out.println("Currently using profile:: "+profile);
+
+		}
+		return null;
 	}
 
 }
---
+++ logic/src/main/java/sk/durovic/configuration/Person.java
@@ -0,0 +1,11 @@
+package sk.durovic.configuration;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Person {
+
+    @Id
+    private int ab;
+}
