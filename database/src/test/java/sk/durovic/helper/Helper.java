98ffa9ab8cfb8927333ce63a61c696d133296d8c
Indigo <durovic.marek@gmail.com>
2022-04-06 20:45:00 +0200

    changed object instance in test Helper class

--- database/src/test/java/sk/durovic/helper/Helper.java
+++ database/src/test/java/sk/durovic/helper/Helper.java
@@ -6,7 +6,7 @@ import java.lang.reflect.Field;
 
 public class Helper {
 
-    public static <T> void setIdOfInstance(T instance, String fieldName, Object value) throws NoSuchFieldException, IllegalAccessException{
+    public static void setIdOfInstance(Object instance, String fieldName, Object value) throws NoSuchFieldException, IllegalAccessException{
         Field field = instance.getClass().getDeclaredField(fieldName);
         field.setAccessible(true);
         field.set(instance, value);
