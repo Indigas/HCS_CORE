3ce6b9557a52ae43ceb5a402fad42a5b0cd7d50b
Indigo <durovic.marek@gmail.com>
2022-10-29 20:58:47 +0200

    jmsworkerprovider change back to public class

--- logic/src/main/java/sk/durovic/jms/messaging/worker/provider/utility/JmsWorkerProviderImpl.java
+++ logic/src/main/java/sk/durovic/jms/messaging/worker/provider/utility/JmsWorkerProviderImpl.java
@@ -3,7 +3,8 @@ package sk.durovic.jms.messaging.worker.provider.utility;
 import sk.durovic.jms.messaging.worker.implementations.JmsPatientWorker;
 import sk.durovic.jms.messaging.worker.provider.JmsWorkerProvider;
 
-class JmsWorkerProviderImpl implements JmsWorkerProvider {
+public class JmsWorkerProviderImpl implements JmsWorkerProvider {
+
     @Override
     public JmsPatientWorker createJmsPatientWorker() {
         return new JmsPatientWorker();
